d850fc60f53340b39a81c849824fe1348e10d1fc
Filipe <fgallo_dev@hotmail.com>
2023-01-23 10:35:18 -0300

    Imutabilidade 2

--- redux-basico/imutabilidade/script.js
+++ redux-basico/imutabilidade/script.js
@@ -1,33 +1,51 @@
-const array1 = [0, 1, 2, 3, 4];
-const array2 = array1.filter((item) => true);
+// const array1 = [0, 1, 2, 3, 4];
+// const array2 = array1.filter((item) => true);
 
 // console.log(array1 === array2);
 // console.log(array1);
 // console.log(array2);
 
+// const obj1 = {
+//   nome: "José",
+//   idade: 49,
+// };
+// const obj2 = Object.assign({}, obj1, { nome: "Maria" });
+
+// console.log(obj1 === obj2);
+// console.log(obj1);
+// console.log(obj2);
+
 const obj1 = {
   nome: "José",
   idade: 49,
 };
-const obj2 = Object.assign({}, obj1, { nome: "Maria" });
 
-console.log(obj1 === obj2);
+const obj2 = immer.produce(obj1, (draft) => {
+  draft.idade = 45;
+});
+
 console.log(obj1);
 console.log(obj2);
 
 const initialState = {
   nome: "Filipe",
-  idade: 22,
+  sobre: {
+    dados: {
+      idade: 31,
+    },
+  },
 };
 
-function reducer(state = initialState, action) {
+const reducer = immer.produce((state, action) => {
   switch (action.type) {
     case "MUDAR_NOME":
-      return { ...state, nome: action.payload };
-    default:
-      return state;
+      state.nome = action.payload;
+      break;
+    case "MUDAR_IDADE":
+      state.sobre.dados.idade = action.payload;
+      break;
   }
-}
+}, initialState);
 
 const store = Redux.createStore(
   reducer,
@@ -37,4 +55,6 @@ const store = Redux.createStore(
 store.dispatch({ type: "MUDAR_NOME", payload: "André" });
 store.dispatch({ type: "MUDAR_NOME", payload: "João" });
 store.dispatch({ type: "MUDAR_NOME", payload: "Pedro" });
+store.dispatch({ type: "MUDAR_IDADE", payload: 20 });
+store.dispatch({ type: "MUDAR_IDADE", payload: 35 });
 store.dispatch({ type: "TESTE", payload: "Maria" });
