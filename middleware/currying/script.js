201f7be9c8c6a23a8bfa5b7bc4b8c2a306870c2d
Filipe <fgallo_dev@hotmail.com>
2023-01-25 12:43:31 -0300

    Currying

---
+++ middleware/currying/script.js
@@ -0,0 +1,17 @@
+const li = Array.from(document.querySelectorAll("li"));
+
+// const getElementAttr = (key) => {
+//   return (el) => {
+//     return el.getAttribute(key);
+//   };
+// };
+const getElementAttr = (key) => (el) => el.getAttribute(key);
+
+const getAttrDataSlide = getElementAttr("data-slide");
+const getAttrId = getElementAttr("id");
+
+const dataSlideList = li.map(getAttrDataSlide);
+const idList = li.map(getAttrId);
+
+console.log(dataSlideList);
+console.log(idList);
