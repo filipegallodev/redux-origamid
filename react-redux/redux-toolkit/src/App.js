440a4e5bbadef1513367c43f1e14b3474db85a10
Filipe <filipegallodev@hotmail.com>
2023-01-27 09:59:48 -0300

    Redux Toolkit

---
+++ react-redux/redux-toolkit/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { incrementar, reduzir } from "./store/contador";
+import { abrir, fechar } from "./store/modal";
+
+function App() {
+  const { contador, modal } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      {modal && <h1>Total: {contador.total}</h1>}
+      <button onClick={() => dispatch(incrementar())}>Incrementar</button>
+      <button onClick={() => dispatch(reduzir())}>Reduzir</button>
+      <button onClick={() => dispatch(abrir())}>Abrir</button>
+      <button onClick={() => dispatch(fechar())}>Fechar</button>
+    </div>
+  );
+}
+
+export default App;
